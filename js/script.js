7dbdb5ccea769e524dc2ff39ab72fe0432271867
Sylvian Brunet <sylvian84@hotmail.fr>
2021-04-30 18:33:50 +0200

    consideration of other files than images

--- js/script.js
+++ js/script.js
@@ -3,13 +3,25 @@ function addPreview(files, container) {
     files.forEach(function(file) {
         var reader = new FileReader();
         reader.onload = function(e) {
-            container.innerHTML += 
-                "<div class='border p-1 m-1'>" +
-                    "<div class='imgContainer'>" +
-                        "<img src='" + e.target.result + "'>" +
-                    "</div>" +
-                    "<div class='text-center imgSubTitle'>" + file.name + "</div>" +
-                "</div>";
+            let fileSplit = file.name.split(".");
+            let extension = fileSplit[ fileSplit.length-1 ];
+            if (['jpg', 'png', 'jpeg'].includes(extension)) {
+                container.innerHTML += 
+                    "<div class='border p-1 m-1'>" +
+                        "<div class='imgContainer'>" +
+                            "<img src='" + e.target.result + "'>" +
+                        "</div>" +
+                        "<div class='text-center imgSubTitle'>" + file.name + "</div>" +
+                    "</div>";
+            } else {
+                container.innerHTML += 
+                    "<div class='border p-1 m-1'>" +
+                        "<div class='imgContainer'>" +
+                            "<i class='bi bi-file-earmark-fill' style='font-size: 35px;'></i>" +
+                        "</div>" +
+                        "<div class='text-center imgSubTitle'>" + file.name + "</div>" +
+                    "</div>";  
+            }
         }
         reader.readAsDataURL(file);
     });
@@ -17,7 +29,7 @@ function addPreview(files, container) {
 
 let dropContainer = document.getElementById('container');
 let fileInput = document.getElementById('fileInput');
-let allowedExtensions = ["jpg", "jpeg", "png"];
+let allowedExtensions = ["jpg", "jpeg", "png", "pdf"];
 
 dropContainer.ondragover = dropContainer.ondragenter = function(e) {
     e.preventDefault();
@@ -27,10 +39,10 @@ dropContainer.ondrop = function(e) {
     let objFiles = e.dataTransfer.files;
     let files = Object.values(objFiles);
     
-    for(let i = 0; i < files.length; i++) {
+    for (let i = 0; i < files.length; i++) {
         let fileSplit = files[i].name.split(".");
         let extension = fileSplit[ fileSplit.length-1 ];
-        if(!allowedExtensions.includes(extension)) {
+        if (!allowedExtensions.includes(extension)) {
             files.splice(i, 1);
         }
     }
